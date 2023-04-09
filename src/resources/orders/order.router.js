ce018873ce3b43791d66aacea75cca2a6172b8d8
xNIGHTMARENIGHTx <vqy008oj1@gmail.com>
2023-04-09 22:18:36 +0300

    feat: done OrderRouter

--- src/resources/orders/order.router.js
+++ src/resources/orders/order.router.js
@@ -1,14 +1,67 @@
 import { Router } from 'express';
+import { StatusCodes } from "http-status-codes"
 
-import User from './order.model.js';
-import * as usersService from './order.service.js';
+import Order from './order.model.js';
+import * as ordersService from './order.service.js';
 
 const router = Router();
 
 router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
+  const users = await ordersService.getAll();
+
+  res.json(users.map(Order.toResponse));
 });
+
+router.route("/").post(async (req, res) => {
+  const { ClientId, TourId, PM } = req.body;
+
+  const order = await ordersService.create({ClientId, TourId, PM});
+
+  if(order){
+    res.status(StatusCodes.CREATED).json(Order.toResponse(order));
+  }
+  else {
+    res.status(StatusCodes.BAD_REQUEST).json({code: `${StatusCodes.BAD_REQUEST}`, msg:"Bad Request"});
+  }
+})
+
+router.route("/:id").get(async (req, res) =>{
+  const { id } = req.params;
+
+  const task = await ordersService.getById(id);
+
+  if(task !== -1){
+    res.status(StatusCodes.OK).json(Order.toResponse(task));
+  }
+  else {
+    res.status(StatusCodes.NOT_FOUND).json({ code:`${StatusCodes.NOT_FOUND}`, msg:"Order Not Found :("});
+  }
+})
+
+router.route("/:id").put(async (req, res) =>{
+  const { id } = req.params;
+  const {ClientId, TourId, PM} = req.body;
+
+  const order = await ordersService.update({id, ClientId, TourId, PM});
+
+  if(order){
+    res.status(StatusCodes.OK).json(Order.toResponse(order));
+  }
+  else{
+    res.status(StatusCodes.NOT_FOUND).json({ code:`${StatusCodes.NOT_FOUND}`, msg:"Order Not Found :("});
+  }
+})
+
+router.route("/:id").delete(async (req, res) =>{
+  const { id } = req.params;
+
+  const order = await ordersService.deleteById(id);
+  if(order){
+    res.status(StatusCodes.NO_CONTENT).json({ code:`${StatusCodes.NO_CONTENT}`, msg:"Order has been Deleted"});
+  }
+  else{
+    res.status(StatusCodes.NOT_FOUND).json({ code:`${StatusCodes.NOT_FOUND}`, msg:"Order Not Found :("});
+  }
+})
 
 export default router;
