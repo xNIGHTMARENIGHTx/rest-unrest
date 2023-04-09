02a423473f121e568b7b4d9547049674bed96741
xNIGHTMARENIGHTx <vqy008oj1@gmail.com>
2023-04-09 22:37:55 +0300

    feat: done ClientServices

--- src/resources/clients/client.service.js
+++ src/resources/clients/client.service.js
@@ -1,5 +1,24 @@
 import * as usersRepo from './client.memory.repository.js';
+import * as odersRepo from "../orders/order.memory.repository.js"
 
 const getAll = () => usersRepo.getAll();
+const getById = (id) => usersRepo.getById(id);
+const createClient= ({name, surname, CT, login, password}) =>
+    usersRepo.create({name, surname, CT, login, password});
+const deleteById = async (id) =>{
+    const user = await usersRepo.getById(id);
+    usersRepo.deleteById(id);
+    odersRepo.deleteByClientId(id);
 
-export { getAll };
+    return user;
+}
+const update = ({id, name, surname, CT, login, password}) =>
+    usersRepo.update({id, name, surname, CT, login, password});
+
+export { 
+    getAll,
+    getById,
+    createClient,
+    deleteById,
+    update 
+};
