8689094882591d6478166889307b247ab137c569
xNIGHTMARENIGHTx <vqy008oj1@gmail.com>
2023-04-09 21:02:03 +0300

    feat: updated ClientMemRep and ClientModel

--- src/resources/clients/client.memory.repository.js
+++ src/resources/clients/client.memory.repository.js
@@ -1,5 +1,46 @@
-const getAll = async () =>
-  // TODO: mock implementation. should be replaced during task development
-  [];
+import User from "./client.model.js";
 
-export { getAll };
+const users = [
+  new User({name: "as",
+  surname: "sa",
+  CT: "coadsa",
+  login: "test1",
+  password: "testtest"
+  })
+]
+
+const getAll = async () => users;
+
+const getById = async (id) => users.find((user) => user.id === id);
+
+const create = async ({name, surname, CT, login, password}) =>{
+  const user = new User({name, surname, CT, login, password});
+  users.push(user);
+}
+
+const deleteById = async (id) => {
+  const pos = users.findIndex((user) => user.id === id);
+  
+  if(pos === -1) return null;
+
+  users.splice(pos,1);
+
+  return null;
+}
+
+const update = async ({id, name, surname, CT, login, password}) =>{
+  const pos = users.findIndex((user) => user.id === id);
+
+  if(pos === -1) return null;
+
+  return {...users[pos], name, surname, CT, login, password}
+}
+
+export { 
+  users,
+  getAll,
+  getById,
+  create,
+  deleteById,
+  update 
+};
