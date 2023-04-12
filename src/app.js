b528fc95c1ce950ed40227b39dcc00d1db1a0adb
xNIGHTMARENIGHTx <vqy008oj1@gmail.com>
2023-04-13 00:49:02 +0300

    feat: added index.html and a picture

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from 'express';
 
+import url from 'url';
+import path from "path"
+
 import clientRouter from './resources/clients/client.router.js';
 import orderRouter from './resources/orders/order.router.js';
 import tourRouter from './resources/tours/tour.router.js';
@@ -7,7 +10,11 @@ import agentRouter from "./resources/agents/agent.router.js";
 
 const app = express();
 
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 app.use(express.json());
+
+app.use("/", express.static(path.join(dirname, "html")));
 
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') {
