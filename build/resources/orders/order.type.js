5ec434947cad761e1ebece9eedefd6e9641d9110
xNIGHTMARENIGHTx <vqy008oj1@gmail.com>
2023-04-27 14:06:10 +0300

    fix: esc -> true

--- build/resources/orders/order.type.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=order.type.js.map
--- build/server.js
+++
@@ -1,5 +0,0 @@
-import config from './common/config.js';
-import app from './app.js';
-const { PORT } = config;
-app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`));
-//# sourceMappingURL=server.js.map
