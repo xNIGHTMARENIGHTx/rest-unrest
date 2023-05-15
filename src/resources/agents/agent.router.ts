1df3795fcd3aee6d786b5b54861570f4033b4b13
xNIGHTMARENIGHTx <vqy008oj1@gmail.com>
2023-05-15 09:07:15 +0300

    feat: add static ips

--- src/resources/agents/agent.router.ts
+++ src/resources/agents/agent.router.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response} from 'express';
+import { Router, Request, Response } from 'express';
 import { StatusCodes } from "http-status-codes";
 
 import Agent from './agent.model.js';
